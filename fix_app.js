5ca022f56f607f9973447631d2324d7f610ee147
Jake <55980231+JakeJia@users.noreply.github.com>
2026-08-10 20:36:05 -0700

    chore: remove temp file

--- fix_app.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.vue', 'utf8');
-code = code.replace(/appWindow\.startDragging\(direction as any\);/g, 'appWindow.startDragging();');
-fs.writeFileSync('src/App.vue', code);
